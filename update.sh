37b13a1642e3d7c2de164124a21eadf4f28f1d23
joelutsi <joel.utsi@ess.eu>
2025-12-21 20:52:03 +0100

    fixed stuff

--- update.sh
+++ update.sh
@@ -2,9 +2,10 @@
 
 #run this once: chmod +x update.sh
 
+cd clock
 git pull
 cd ..
 scp clock/clock.cc rpi-rgb-led-matrix/
 cd rpi-rgb-led-matrix/
 g++ -O2 -std=c++17 clock.cc -o clock -Iinclude -Llib -lrgbmatrix -lpthread
-./clock -D0 --led-no-hardware-pulse --led-cols=64 --led-rows=32
+sudo reboot

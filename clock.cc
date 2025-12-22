1cfed513dfc210952cd232e543a3735944505745
joelutsi <joel.utsi@ess.eu>
2025-12-22 23:41:14 +0100

    god jul

--- clock.cc
+++ clock.cc
@@ -200,14 +200,14 @@ int main(int argc, char *argv[]) {
   if (!small_font.LoadFont("fonts/6x10.bdf")) return 3;
   if (!big_font.LoadFont("fonts/10x20.bdf")) return 4;
 
-const Color white (255, 255, 255);   
-const Color black (0,   0,   0);     
-const Color red   (0,   255, 0);    
-const Color green (0,   0,   255); 
-const Color blue  (255, 0,   0);     
-const Color orange(0,   255, 165);   
-const Color grey  (128, 128, 128);   
-const Color brown (19,  139, 69);    
+  const Color white (255, 255, 255);  
+  const Color black (0,   0,   0);  
+  const Color red   (0,   0,   255);
+  const Color green (0,   255, 0); 
+  const Color blue  (255, 0,   0);   
+  const Color orange(0,   165, 255);
+  const Color grey  (128, 128, 128);   
+  const Color brown (19,  69,  139);
 
   const int x_text = 5;
   const int y_text = 20;

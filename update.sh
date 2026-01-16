f39307d93658b5f22849c7ebd255d49e04726297
joelutsi <joel.utsi@ess.eu>
2026-01-16 08:49:12 +0100

    Changes made

--- update.sh
+++ update.sh
@@ -2,8 +2,16 @@
 
 #run this once: chmod +x update.sh
 
-cd /home/joel/clock
-git pull
-scp /home/joel/clock/clock.cc /home/joel/rpi-rgb-led-matrix/
-cd /home/joel/rpi-rgb-led-matrix/
-g++ -O2 -std=c++17 clock.cc -o clock -Iinclude -Llib -lrgbmatrix -lpthread
+for i in {1..4}; do
+    if ping -c 1 -W 2 8.8.8.8 &> /dev/null; then
+        cd /home/joel/clock
+        
+        if git pull; then
+            scp /home/joel/clock/clock.cc /home/joel/rpi-rgb-led-matrix/
+            cd /home/joel/rpi-rgb-led-matrix/
+            g++ -O2 -std=c++17 clock.cc -o clock -Iinclude -Llib -lrgbmatrix -lpthread
+        fi
+        break
+    fi
+    sleep 5
+done

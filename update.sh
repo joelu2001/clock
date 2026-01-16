6bcbd57aa5b7053d0794042e83a13ff377c53e1d
joelutsi <joel.utsi@ess.eu>
2026-01-16 08:02:23 +0100

    Changes made

--- update.sh
+++ update.sh
@@ -7,4 +7,3 @@ git pull
 scp /home/joel/clock/clock.cc /home/joel/rpi-rgb-led-matrix/
 cd /home/joel/rpi-rgb-led-matrix/
 g++ -O2 -std=c++17 clock.cc -o clock -Iinclude -Llib -lrgbmatrix -lpthread
-/home/joel/rpi-rgb-led-matrix/clock -D0 --led-rgb-sequence=RGB --led-no-hardware-pulse --led-cols=64 --led-ro>

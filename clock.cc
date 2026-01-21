f9d8c12237f637561ba0c9e9aa7c6e0f8b761cb1
joelutsi <joel.utsi@ess.eu>
2026-01-21 12:44:03 +0100

    Changes made

--- clock.cc
+++ clock.cc
@@ -222,7 +222,7 @@ int main(int argc, char *argv[]) {
 
     offscreen = matrix->SwapOnVSync(offscreen);
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    std::this_thread::sleep_for(std::chrono::milliseconds(8640000));
   }
 
   offscreen->Clear();
